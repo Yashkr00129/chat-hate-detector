e8deead8b079a60dc34747b33e8d029f47e2bd4d
joshkatz1 <joshkatz1@gmail.com>
2022-12-28 18:29:03 +0200

    change apoolo varibales

--- fronrend/src/graphql/apollo-client.ts
+++ fronrend/src/graphql/apollo-client.ts
@@ -7,23 +7,23 @@ import { getSession } from "next-auth/react";
 const __DEV__ = process.env.NODE_ENV !== "production";
 
 const httpLink = new HttpLink({
-  uri: __DEV__
-    ? "http://localhost:4000/graphql"
-    : "chat-hate-detector-production.up.railway.app/graphql",
+  uri: DEV
+  ? "http://localhost:4000/graphql"
+  : process.env.API_URL,
   credentials: "include",
 });
-
+  
 const wsLink =
-  typeof window != "undefined"
-    ? new GraphQLWsLink(
-        createClient({
-          url: __DEV__
-            ? "ws://localhost:4000/graphql/subscriptions"
-            : "ws://chat-hate-detector-production.up.railway.app/graphql/subscriptions",
-          connectionParams: async () => ({ session: await getSession() }),
-        })
-      )
-    : null;
+typeof window != "undefined"
+? new GraphQLWsLink(
+createClient({
+url: DEV
+? "ws://localhost:4000/graphql/subscriptions"
+: process.env.WS_API_URL,
+connectionParams: async () => ({ session: await getSession() }),
+})
+)
+: null;
 
 const link =
   typeof window != "undefined" && wsLink != null
