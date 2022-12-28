f6e6532fc32483a1f0834a2f58f7c0f66e5fca0c
joshkatz1 <joshkatz1@gmail.com>
2022-12-28 20:05:11 +0200

    uri

--- fronrend/src/graphql/apollo-client.ts
+++ fronrend/src/graphql/apollo-client.ts
@@ -7,9 +7,10 @@ import { getSession } from "next-auth/react";
 const __DEV__ = process.env.NODE_ENV !== "production";
 
 const httpLink = new HttpLink({
-  uri: __DEV__
-  ? "http://localhost:4000/graphql"
-  : process.env.API_URL,
+  uri:process.env.API_URL,
+  // uri: __DEV__
+  // ? "http://localhost:4000/graphql"
+  // : process.env.API_URL,
   credentials: "include",
 });
   
