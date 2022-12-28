2986bb6efa080e980126468533f588aaa04b400f
yash sharma <yashkr00129@gmail.com>
2022-12-28 17:00:11 +0530

    Change URLS in the frontend

--- fronrend/src/graphql/apollo-client.ts
+++ fronrend/src/graphql/apollo-client.ts
@@ -1,35 +1,46 @@
-import { ApolloClient, HttpLink, InMemoryCache,split } from '@apollo/client';
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { getSession } from 'next-auth/react';
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { getSession } from "next-auth/react";
+
+const __DEV__ = process.env.NODE_ENV !== "production";
 
 const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql',
-    credentials: 'include',
-
+  uri: __DEV__
+    ? "http://localhost:4000/graphql"
+    : "chat-hate-detector-production.up.railway.app/graphql",
+  credentials: "include",
 });
-const wsLink = typeof window!='undefined' ?  new GraphQLWsLink(createClient({
-    url: 'ws://localhost:4000/graphql/subscriptions',
-    connectionParams: async () => ({session: await getSession()})
-  })) : null;
 
-  const link = typeof window!='undefined' && wsLink!= null ? split(
-    ({ query }) => {
-      const definition = getMainDefinition(query);
-      return (
-        definition.kind === 'OperationDefinition' &&
-        definition.operation === 'subscription'
-      );
-    },
-    wsLink,
-    httpLink,
-  ): httpLink;
+const wsLink =
+  typeof window != "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: __DEV__
+            ? "ws://localhost:4000/graphql/subscriptions"
+            : "ws://chat-hate-detector-production.up.railway.app/graphql/subscriptions",
+          connectionParams: async () => ({ session: await getSession() }),
+        })
+      )
+    : null;
+
+const link =
+  typeof window != "undefined" && wsLink != null
+    ? split(
+        ({ query }) => {
+          const definition = getMainDefinition(query);
+          return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+          );
+        },
+        wsLink,
+        httpLink
+      )
+    : httpLink;
 
 export const client = new ApolloClient({
-    link,
-    cache: new InMemoryCache(),
-})
-  
-
-
+  link,
+  cache: new InMemoryCache(),
+});
