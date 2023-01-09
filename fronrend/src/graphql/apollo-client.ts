deecda8bb2705303a0b6de80fbb4d1358edabc4d
yash sharma <yashkr00129@gmail.com>
2023-01-09 06:02:51 +0530

    Change Origin

--- fronrend/src/graphql/apollo-client.ts
+++ fronrend/src/graphql/apollo-client.ts
@@ -1,13 +1,13 @@
-import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { createClient } from "graphql-ws";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { getSession } from "next-auth/react";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client"
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
+import { createClient } from "graphql-ws"
+import { getMainDefinition } from "@apollo/client/utilities"
+import { getSession } from "next-auth/react"
 
 const httpLink = new HttpLink({
   uri: "http://3.83.30.115/graphql",
   credentials: "include",
-});
+})
 
 const wsLink =
   typeof window != "undefined"
@@ -23,19 +23,19 @@ const link =
   typeof window != "undefined" && wsLink != null
     ? split(
         ({ query }) => {
-          const definition = getMainDefinition(query);
+          const definition = getMainDefinition(query)
           return (
             definition.kind === "OperationDefinition" &&
             definition.operation === "subscription"
-          );
+          )
         },
         wsLink,
         httpLink
       )
-    : httpLink;
+    : httpLink
 
 export const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
   credentials: "include",
-});
+})
